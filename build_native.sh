dd77dde3e31f0bfac91f12515288be2e461f6b5a
Angie <angheloalf95@gmail.com>
2022-11-08 18:45:11 -0300

    remove warnings for now

--- build_native.sh
+++ build_native.sh
@@ -1,1 +1,1 @@
-gcc -o cc.o -c src/7.1/cc/cc.c -I include/ -I include/indy_stubs -D NATIVE_BUILD -D_LANGUAGE_C   2> errors.txt
+gcc -o cc.o -c src/7.1/cc/cc.c -I include/ -I include/indy_stubs -D NATIVE_BUILD -D_LANGUAGE_C -w  2> errors.txt
