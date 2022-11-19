28c194d4cf5d809c42dd9c1bbbc1d5ea286e7723
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-11-19 22:40:07 +0000

    Add static to release id

--- src/5.3/releaseid.c
+++ src/5.3/releaseid.c
@@ -1,1 +1,1 @@
-const char __Release_ID[] = "@(#)$Header: IRIX 5.3:1021572033 built 11/03/94 at pigheart:/joist/5.3MR/root $";
+static const char __Release_ID[] = "@(#)$Header: IRIX 5.3:1021572033 built 11/03/94 at pigheart:/joist/5.3MR/root $";
