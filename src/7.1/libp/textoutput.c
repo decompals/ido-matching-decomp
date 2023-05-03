5a99077f35b3497e1b6e68b092733763591a342a
angie <angheloalf95@gmail.com>
2023-05-03 12:26:13 -0400

    fix textoutput warnings and clean it up

--- src/7.1/libp/textoutput.c
+++ src/7.1/libp/textoutput.c
@@ -12,12 +12,12 @@ static void func_00471400(FILE* file, char* string, int arg2) {
             file->_cnt += arg2;
             if (file->_cnt > 0) {
                 arg2 -= file->_cnt;
-                ptr = file->_ptr;
+                ptr = (char*)file->_ptr;
                 end = file->_cnt + string;
                 do {
                     *ptr++ = *string++;
                 } while (string != end);
-                file->_ptr = ptr;
+                file->_ptr = (unsigned char*)ptr;
             }
 
             file->_cnt = -1;
@@ -29,17 +29,17 @@ static void func_00471400(FILE* file, char* string, int arg2) {
             file->_cnt -= arg2;
         }
 
-        ptr = file->_ptr;
+        ptr = (char*)file->_ptr;
         end = string + arg2;
         do {
             *ptr++ = *string++;
         } while (string != end);
-        file->_ptr = ptr;
+        file->_ptr = (unsigned char*)ptr;
     }
 }
 
 // pad
-static void func_00471580(FILE *file, int ch, int arg2) {
+static void func_00471580(FILE* file, int ch, int arg2) {
     char* ptr;
     char* end;
 
@@ -49,12 +49,12 @@ static void func_00471580(FILE *file, int ch, int arg2) {
             file->_cnt += arg2;
             if (file->_cnt > 0) {
                 arg2 -= file->_cnt;
-                ptr = file->_ptr;
+                ptr = (char*)file->_ptr;
                 end = file->_cnt + ptr;
                 do {
                     *ptr++ = ch;
                 } while (ptr != end);
-                file->_ptr = ptr;
+                file->_ptr = (unsigned char*)ptr;
             }
 
             file->_cnt = -1;
@@ -66,16 +66,16 @@ static void func_00471580(FILE *file, int ch, int arg2) {
             file->_cnt -= arg2;
         }
 
-        ptr = file->_ptr;
+        ptr = (char*)file->_ptr;
         end = ptr + arg2;
         do {
             *ptr++ = ch;
         } while (ptr != end);
-        file->_ptr = ptr;
+        file->_ptr = (unsigned char*)ptr;
     }
 }
 
-void writeln(FILE *file) {
+void writeln(FILE* file) {
     if (!(file->_flag & 2)) {
         fprintf(stderr, "writeln called on file not open for writing.\n");
         return;
@@ -83,11 +83,11 @@ void writeln(FILE *file) {
     putc_locked('\n', file);
 }
 
-void page(FILE *file) {
+void page(FILE* file) {
     putc_locked('\f', file);
 }
 
-void write_char(FILE *file, char ch, int arg2) {
+void write_char(FILE* file, char ch, int arg2) {
     if (arg2 >= 2) {
         func_00471580(file, ' ', arg2 - 1);
         putc_locked(ch, file);
@@ -99,17 +99,17 @@ void write_char(FILE *file, char ch, int arg2) {
     }
 }
 
-void write_string(FILE *file, char *string, unsigned int arg2, int arg3) {
+void write_string(FILE* file, char* string, size_t arg2, int arg3) {
     char* ptr;
 
     if (arg3 == 0) {
         ptr = string + arg2;
-        while ((ptr != string) && (ptr[-1] == 0x20)) {
+        while ((ptr != string) && (ptr[-1] == ' ')) {
             ptr--;
         }
         arg3 = ptr - string;
-    } else if (arg2 < (unsigned int)arg3) {
-        func_00471580(file, 0x20, arg3 - arg2);
+    } else if (arg2 < (size_t)arg3) {
+        func_00471580(file, ' ', arg3 - arg2);
         arg3 = arg2;
     }
     if (arg3 > 0) {
@@ -117,7 +117,7 @@ void write_string(FILE *file, char *string, unsigned int arg2, int arg3) {
     }
 }
 
-void write_enum(FILE *file, int arg1, char *arg2, int arg3) {
+void write_enum(FILE* file, int arg1, char* arg2, int arg3) {
     int sp24 = arg1;
 
     while (arg1 != 0) {
