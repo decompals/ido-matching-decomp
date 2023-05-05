38150103e11d892c668fef965df44eb50bc0fed6
LLONSIT <lonsitsyt456@gmail.com>
2023-05-05 15:51:59 -0500

    Delete xmalloc files

--- src/5.3/xmalloc/xmalloc.h
+++
@@ -1,9 +0,0 @@
-type
-AllocBlock_ptr =  ^AllocBlock;
-AllocBlock = record;
-    prev: AllocBlock_ptr;
-    next : AllocBlock_ptr;
-    item_list : AllocBlock_ptr;
-    size : integer;
-    /*struct AllocItem data[1];*/
-end;
