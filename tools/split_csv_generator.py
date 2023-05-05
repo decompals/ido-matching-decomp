0bee7d5eb109063a744cb88e62549a3e067b0d68
angie <angheloalf95@gmail.com>
2023-05-05 14:58:11 -0400

    Tool to generate `.split.csv` file

---
+++ tools/split_csv_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+import spimdisasm
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("elf_program", help="")
+
+    args = parser.parse_args()
+
+    elfPath = Path(args.elf_program)
+    programName = elfPath.name
+
+    elfBytes = spimdisasm.common.Utils.readFileAsBytearray(elfPath)
+    elfFile = spimdisasm.elf32.Elf32File(elfBytes)
+
+    for header in elfFile.sectionHeaders:
+        sectionType = spimdisasm.elf32.Elf32SectionHeaderType.fromValue(header.type)
+        sectionName = elfFile.shstrtab[header.name]
+
+        if sectionType == spimdisasm.elf32.Elf32SectionHeaderType.PROGBITS:
+            if sectionName in {".text", ".data", ".rodata"}:
+                print(f"offset,vram,{sectionName}")
+                print(f"{header.offset:06X},{header.addr:06X},{programName}")
+            else:
+                print(f"offset,vram,.dummy")
+                print(f"{header.offset:06X},{header.addr:06X},{sectionName[1:]}")
+            print()
+
+        elif sectionType == spimdisasm.elf32.Elf32SectionHeaderType.NOBITS:
+
+            print(f"offset,vram,{sectionName}")
+            print(f"{header.offset:06X},{header.addr:06X},{programName}")
+            print()
+
+            offsetEnd = header.offset + header.size
+            addrEnd = header.addr + header.size
+            print(f"{offsetEnd:06X},{addrEnd:06X},.end")
+            print()
+
+
+if __name__ == "__main__":
+    main()
