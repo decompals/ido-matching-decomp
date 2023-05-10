6c5f4f5966902620385369c4ccf09d372cae91fb
angie <angheloalf95@gmail.com>
2023-05-10 12:33:52 -0400

    csv_generator improvements
    - Add section offset to the progname
    - Fail early if can't open elf file
    - Comment out sections after NOBITS

--- tools/split_csv_generator.py
+++ tools/split_csv_generator.py
@@ -15,7 +15,10 @@ def main():
     programName = elfPath.name
 
     elfBytes = spimdisasm.common.Utils.readFileAsBytearray(elfPath)
+    assert len(elfBytes) > 0, f"Could not read file {elfPath}"
     elfFile = spimdisasm.elf32.Elf32File(elfBytes)
+
+    foundEnd = False
 
     for header in elfFile.sectionHeaders:
         sectionType = spimdisasm.elf32.Elf32SectionHeaderType.fromValue(header.type)
@@ -23,22 +26,28 @@ def main():
 
         if sectionType == spimdisasm.elf32.Elf32SectionHeaderType.PROGBITS:
             if sectionName in {".text", ".data", ".rodata"}:
+                print(f"{'# ' if foundEnd else ''}", end="")
                 print(f"offset,vram,{sectionName}")
-                print(f"{header.offset:06X},{header.addr:08X},{programName}")
+                print(f"{'# ' if foundEnd else ''}", end="")
+                print(f"{header.offset:06X},{header.addr:08X},{programName}_{header.offset:06X}")
             else:
+                print(f"{'# ' if foundEnd else ''}", end="")
                 print(f"offset,vram,.dummy")
+                print(f"{'# ' if foundEnd else ''}", end="")
                 print(f"{header.offset:06X},{header.addr:08X},{sectionName[1:]}")
             print()
 
         elif sectionType == spimdisasm.elf32.Elf32SectionHeaderType.NOBITS:
-
+            # Assumes bss will be the last section. Comments out any other PROGBITS section after it
             print(f"offset,vram,{sectionName}")
-            print(f"{header.offset:06X},{header.addr:08X},{programName}")
+            print(f"{header.offset:06X},{header.addr:08X},{programName}_{header.addr:06X}")
             print()
 
             offsetEnd = header.offset + header.size
             addrEnd = header.addr + header.size
             print(f"{offsetEnd:06X},{addrEnd:08X},.end")
+
+            foundEnd = True
 
 
 if __name__ == "__main__":
