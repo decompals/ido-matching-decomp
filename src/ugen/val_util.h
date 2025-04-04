d958940dbdee2b96762657c8ca2adf164a52bb46
Unnunu <serge_zorkin@mail.ru>
2025-04-04 15:00:24 +0300

    match val_util.p

--- src/ugen/val_util.h
+++ src/ugen/val_util.h
@@ -1,6 +1,22 @@
 #ifndef VAL_UTIL_H
 #define VAL_UTIL_H
-procedure emit_val(arg0: integer; arg1: u8; var arg2: Valu; arg3: cardinal); external;
-procedure emit_label_val(arg0: integer; arg1: integer; arg2: integer; arg3: cardinal); external;
-function find_val_type(dtype: Datatype; size: cardinal): u8; external;
+
+type
+    ValType = (
+        ValType_Null,
+        ValType_Byte,
+        ValType_Half,
+        ValType_Word,
+        ValType_Dword,
+        ValType_Float,
+        ValType_Double,
+        ValType_Ascii,
+        ValType_Label,
+        ValType_Set
+    );
+
+procedure emit_val(arg0: integer; vtype: ValType; var value: Valu; rep: cardinal); external;
+procedure emit_label_val(arg0: integer; symno: integer; value: integer; size: cardinal); external;
+function find_val_type(dtype: Datatype; size: integer): ValType; external;
+
 #endif /* VAL_UTIL_H */
