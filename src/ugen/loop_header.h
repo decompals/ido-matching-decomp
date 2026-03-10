9a2923455b79fefebceea62e4ee915a54f23cc08
Unnunu <serge_zorkin@mail.ru>
2026-03-10 16:04:31 +0300

    add missing headers

---
+++ src/ugen/loop_header.h
@@ -0,0 +1,7 @@
+#ifndef LOOP_HEADER_H
+#define LOOP_HEADER_H
+
+procedure cross_jump(arg0 : Ptree);
+external;
+
+#endif /* LOOP_HEADER_H */
---
+++ src/ugen/tree_print.h
@@ -0,0 +1,7 @@
+#ifndef TREE_PRINT_H
+#define TREE_PRINT_H
+
+procedure print_tree(var f : Text; arg1 : ^tree; arg2 : cardinal; arg3 : cardinal);
+external;
+
+#endif /* TREE_PRINT_H */
